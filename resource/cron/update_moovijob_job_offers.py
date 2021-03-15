6a832234af7e1b5862dc7981127fe9c03c4b3aac
Alexis <alexis.prunier@securitymadein.lu>
2021-03-15 15:56:28 +0100

    Add company tags on Moovijob offers + Archive deprecated offers

--- resource/cron/update_moovijob_job_offers.py
+++ resource/cron/update_moovijob_job_offers.py
@@ -9,6 +9,7 @@ import json
 from datetime import datetime
 import re
 import copy
+from sqlalchemy import func
 
 
 class UpdateMoovijobJobOffers(Resource):
@@ -28,30 +29,55 @@ class UpdateMoovijobJobOffers(Resource):
         response = request.urlopen(base_url)  # nosec
         data = json.loads(response.read())
 
+        external_references = []
+
         nb_pages = data["last_page"] if "last_page" in data and isinstance(data["last_page"], int) else None
         current_page = 1
-        nb_reviewed, nb_created, nb_modified = 0, 0, 0
+        count = {
+            "reviewed": 0,
+            "created": 0,
+            "modified": 0,
+            "deactivated": 0
+        }
 
         if nb_pages is None:
             return [], "500 No article has been treated"
 
-        while current_page <= nb_pages:
+        # Get the Moovijob company
 
-            external_references = [str(a["id"]) for a in data["data"]]
-            db_articles = self.db.get(self.db.tables["Article"], {"external_reference": external_references})
+        company = self.db.session.query(self.db.tables["Company"]) \
+            .filter(func.lower(self.db.tables["Company"].name).like("%moovijob%")) \
+            .all()
+
+        if len(company) == 0:
+            return [], "500 Moovijob company not found"
+        if len(company) > 1:
+            return [], "500 Too many Moovijob company found"
+
+        company = company[0]
+
+        # process page
+
+        while current_page <= nb_pages:
+            external_references += [str(a["id"]) for a in data["data"]]
+
+            db_articles = self.db.get(
+                self.db.tables["Article"],
+                {"external_reference": [str(a["id"]) for a in data["data"]]}
+            )
 
             for source_article in data["data"]:
                 db_article = [a for a in db_articles if str(source_article["id"]) == a.external_reference]
                 db_article = db_article[0] if len(db_article) > 0 else self.db.tables["Article"]()
 
-                nb_reviewed += 1
-                nb_created += 1 if db_article.id is None else 0
+                count["reviewed"] += 1
+                count["created"] += 1 if db_article.id is None else 0
 
-                db_article, m1 = self._manage_article(db_article, source_article)
+                db_article, m1 = self._manage_article(db_article, source_article, company)
                 db_article_version, m2 = self._manage_article_version(db_article)
                 _, m3 = self._manage_article_version_box(db_article_version, source_article)
 
-                nb_modified += 1 if (db_article.id is not None and (m1 or m2 or m3)) else 0
+                count["modified"] += 1 if (db_article.id is not None and (m1 or m2 or m3)) else 0
 
             # Prepare for next page
 
@@ -61,10 +87,16 @@ class UpdateMoovijobJobOffers(Resource):
                 response = request.urlopen(f"{base_url}&page={current_page}")  # nosec
                 data = json.loads(response.read())
 
-        return "", f"200 Success: {nb_reviewed} treated, {nb_created} created, {nb_modified} modified"
+        # Deactivate the missing offers
 
-    def _manage_article(self, a, source):
+        self._deactivate_deprecated_offers(company, external_references)
 
+        # Send response
+
+        return "", f"200 Success: {count['reviewed']} treated, {count['created']} created, " \
+                   f"{count['modified']} modified, {count['deactivated']} deactivated"
+
+    def _manage_article(self, a, source, company):
         copied_a = copy.deepcopy(a)
 
         today = datetime.today().strftime("%Y-%m-%d")
@@ -87,10 +119,16 @@ class UpdateMoovijobJobOffers(Resource):
         article = self.db.merge(a, self.db.tables["Article"])
         is_modified = not self.db.are_objects_equal(a, copied_a, self.db.tables["Article"])
 
+        # Add the Moovijob relationship if it does not exist
+
+        tags = self.db.get(self.db.tables["ArticleCompanyTag"], {"company": company.id, "article": article.id})
+
+        if len(tags) == 0:
+            self.db.insert({"company": company.id, "article": article.id}, self.db.tables["ArticleCompanyTag"])
+
         return article, is_modified
 
     def _manage_article_version(self, a):
-
         versions = self.db.get(self.db.tables["ArticleVersion"], {"article_id": a.id, "is_main": True})
 
         if len(versions) == 0:
@@ -121,6 +159,24 @@ class UpdateMoovijobJobOffers(Resource):
             return boxes[0].content, False
 
         return None, False
+
+    def _deactivate_deprecated_offers(self, company, external_references):
+        subquery = self.db.session.query(self.db.tables["ArticleCompanyTag"]) \
+            .with_entities(self.db.tables["ArticleCompanyTag"].article) \
+            .filter(self.db.tables["ArticleCompanyTag"].company == company.id) \
+            .subquery()
+
+        offers_to_archive = self.db.session.query(self.db.tables["Article"]) \
+            .filter(self.db.tables["Article"].status == "PUBLIC") \
+            .filter(self.db.tables["Article"].id.in_(subquery)) \
+            .filter(self.db.tables["Article"].external_reference.notin_(external_references)) \
+            .all()
+
+        if len(offers_to_archive) > 0:
+            for o in offers_to_archive:
+                o.status = "ARCHIVE"
+
+            self.db.merge(offers_to_archive, self.db.tables["Article"])
 
     @staticmethod
     def _get_preferred_lang_info(lang_dict):
