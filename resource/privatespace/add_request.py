d6be88b03009fd8734da8a60e401610831682c6e
Alexis <alexis.prunier@securitymadein.lu>
2021-04-16 16:57:24 +0200

    Add image on requests

--- resource/privatespace/add_request.py
+++ resource/privatespace/add_request.py
@@ -1,9 +1,13 @@
 from flask_restful import Resource
 from flask import request
+import io
+import base64
+import traceback
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from decorator.verify_payload import verify_payload
 from decorator.catch_exception import catch_exception
 from decorator.log_request import log_request
+from PIL import Image
 
 
 class AddRequest(Resource):
@@ -14,15 +18,36 @@ class AddRequest(Resource):
     @log_request
     @verify_payload([
         {'field': 'request', 'type': str},
+        {'field': 'image', 'type': str, 'nullable': True, 'optional': True},
     ])
     @jwt_required
     @catch_exception
     def post(self):
         input_data = request.get_json()
+        image = None
+
+        # Control image
+
+        if "image" in input_data and input_data["image"] is not None:
+            try:
+                image = base64.b64decode(input_data["image"].split(",")[-1])
+                image_io = io.BytesIO(image)
+                pil_image = Image.open(image_io)
+            except Exception:
+                traceback.print_exc()
+                return "", "422 Impossible to read the image"
+
+            if pil_image.size[0] > 500 or float(pil_image.size[1]) > 500:
+                return "", "422 Image width and height can't be bigger than 500 pixels"
+
+            image = base64.b64decode(input_data["image"].split(",")[-1])
+
+        # Insert request
 
         user_request = {
             "user_id": int(get_jwt_identity()),
-            "request": input_data["request"]
+            "request": input_data["request"],
+            "image": image,
         }
 
         self.db.insert(user_request, self.db.tables["UserRequest"])
