5226f666b89e4d80cb88bed8929417943c3bca31
Alexis <alexis.prunier@securitymadein.lu>
2021-04-30 16:22:31 +0200

    Add ENUM on request type + Possibility to get "global" request only

--- resource/private/get_my_requests.py
+++ resource/private/get_my_requests.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource
+from flask import request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from db.db import DB
 from decorator.catch_exception import catch_exception
@@ -15,11 +16,17 @@ class GetMyRequests(Resource):
     @jwt_required
     @catch_exception
     def get(self):
+        input_data = request.args.to_dict()
 
-        data = self.db.get(self.db.tables["UserRequest"], {
+        params = {
             "user_id": int(get_jwt_identity()),
             "status": ['NEW', 'IN PROCESS']
-        })
+        }
+
+        if "global_only" in input_data and input_data["global_only"] == "true":
+            params["company_id"] = None
+
+        data = self.db.get(self.db.tables["UserRequest"], params)
         data = Serializer.serialize(data, self.db.tables["UserRequest"])
 
         return data, "200 "
