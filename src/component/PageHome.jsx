9e5915c97dfaa097113f07b7898d0482c43332be
Alexis <alexis.prunier@securitymadein.lu>
2021-06-14 10:58:19 +0200

    Set the link for main cyberlux platform on home page

--- src/component/PageHome.jsx
+++ src/component/PageHome.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./PageHome.css";
 import { Link } from "react-router-dom";
 import Loading from "./box/Loading.jsx";
-import { getEcosystemAppURL } from "../utils/env.jsx";
+import { getEcosystemAppURL, getGlobalAppURL } from "../utils/env.jsx";
 
 export default class PageHome extends React.Component {
 	constructor(props) {
@@ -27,7 +27,7 @@ export default class PageHome extends React.Component {
 					</div>
 
 					<div className="col-md-6">
-						{/* <a
+						<a
 							href={getGlobalAppURL() + "news"}
 							target="_blank"
 							rel="noreferrer"
@@ -36,15 +36,11 @@ export default class PageHome extends React.Component {
 								<i className="far fa-newspaper"/>
 								<h3>News</h3>
 							</div>
-						</a> */}
-						<div className="PageHome-white-block">
-							<i className="far fa-newspaper"/>
-							<h3>Coming soon!</h3>
-						</div>
+						</a>
 					</div>
 
 					<div className="col-md-6">
-						{/* <a
+						<a
 							href={getGlobalAppURL() + "calendar"}
 							target="_blank"
 							rel="noreferrer"
@@ -53,11 +49,7 @@ export default class PageHome extends React.Component {
 								<i className="fas fa-calendar-alt"/>
 								<h3>Events</h3>
 							</div>
-						</a> */}
-						<div className="PageHome-white-block">
-							<i className="fas fa-calendar-alt"/>
-							<h3>Coming soon!</h3>
-						</div>
+						</a>
 					</div>
 
 					<div className="col-md-6">
