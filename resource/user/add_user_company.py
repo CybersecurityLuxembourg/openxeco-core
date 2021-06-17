8ec7cd3b4ace8a3038d9b824c9adc15a4d88cc47
Alexis <alexis.prunier@securitymadein.lu>
2021-06-17 19:01:46 +0200

    Feed technical documentation for some of user resources

--- resource/user/add_user_company.py
+++ resource/user/add_user_company.py
@@ -1,12 +1,12 @@
 from flask_restful import Resource
 from flask_apispec import MethodResource
-from flask import request
 from flask_jwt_extended import jwt_required
-from decorator.verify_payload import verify_payload
 from decorator.verify_admin_access import verify_admin_access
 from decorator.catch_exception import catch_exception
 from decorator.log_request import log_request
 from exception.object_already_existing import ObjectAlreadyExisting
+from webargs import fields
+from flask_apispec import use_kwargs, doc
 
 
 class AddUserCompany(MethodResource, Resource):
@@ -15,19 +15,24 @@ class AddUserCompany(MethodResource, Resource):
         self.db = db
 
     @log_request
-    @verify_payload([
-        {'field': 'user', 'type': int},
-        {'field': 'company', 'type': int},
-    ])
+    @doc(tags=['user'],
+         description='Add an assignment of a user to a company',
+         responses={
+             "200": {},
+             "422.a": {"description": "Object already existing"},
+         })
+    @use_kwargs({
+        'user': fields.Int(),
+        'company': fields.Int(),
+    })
     @jwt_required
     @verify_admin_access
     @catch_exception
-    def post(self):
-        input_data = request.get_json()
+    def post(self, **kwargs):
 
         row = {
-            "user_id": input_data["user"],
-            "company_id": input_data["company"],
+            "user_id": kwargs["user"],
+            "company_id": kwargs["company"],
         }
 
         if len(self.db.get(self.db.tables["UserCompanyAssignment"], row)) > 0:
--- resource/user/add_user_group.py
+++ resource/user/add_user_group.py
@@ -1,11 +1,11 @@
 from flask_restful import Resource
 from flask_apispec import MethodResource
-from flask import request
 from flask_jwt_extended import jwt_required
-from decorator.verify_payload import verify_payload
 from decorator.verify_admin_access import verify_admin_access
 from decorator.catch_exception import catch_exception
 from decorator.log_request import log_request
+from webargs import fields
+from flask_apispec import use_kwargs, doc
 
 
 class AddUserGroup(MethodResource, Resource):
@@ -14,15 +14,19 @@ class AddUserGroup(MethodResource, Resource):
         self.db = db
 
     @log_request
-    @verify_payload([
-        {'field': 'name', 'type': str},
-    ])
+    @doc(tags=['user'],
+         description='Add a user group',
+         responses={
+             "200": {},
+         })
+    @use_kwargs({
+        'name': fields.Str(),
+    })
     @jwt_required
     @verify_admin_access
     @catch_exception
-    def post(self):
-        input_data = request.get_json()
+    def post(self, **kwargs):
 
-        self.db.insert({"name": input_data["name"]}, self.db.tables["UserGroup"])
+        self.db.insert({"name": kwargs["name"]}, self.db.tables["UserGroup"])
 
         return "", "200 "
