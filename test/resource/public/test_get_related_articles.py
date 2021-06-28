c1d2962f557e88ecb694d54732d524dc28656860
Alexis <alexis.prunier@securitymadein.lu>
2021-06-28 12:41:52 +0200

    Adapt unittest to "Remove media column on Article table and adapt resources"

--- test/resource/public/test_get_related_articles.py
+++ test/resource/public/test_get_related_articles.py
@@ -61,7 +61,6 @@ class TestGetRelatedArticles(BaseCase):
                 'id': 2,
                 'image': None,
                 'link': None,
-                'media': 'ALL',
                 'publication_date': (datetime.date.today() - datetime.timedelta(days=2)).strftime("%Y-%m-%d"),
                 'start_date': None,
                 'status': 'PUBLIC',
@@ -76,7 +75,6 @@ class TestGetRelatedArticles(BaseCase):
                 'id': 3,
                 'image': None,
                 'link': None,
-                'media': 'ALL',
                 'publication_date': (datetime.date.today() - datetime.timedelta(days=3)).strftime("%Y-%m-%d"),
                 'start_date': None,
                 'status': 'PUBLIC',
@@ -168,90 +166,6 @@ class TestGetRelatedArticles(BaseCase):
 
         self.assertEqual(200, response.status_code)
         self.assertEqual([], response.json)
-
-    def test_ok_with_media(self):
-
-        # Insertion of articles
-        self.db.insert({
-            "id": 1,
-            "title": "TITLE1",
-            "handle": "TITLE1",
-            "status": "PUBLIC",
-            "media": "SECURITYMADEIN.LU",
-            "publication_date": datetime.date.today() - datetime.timedelta(days=1)
-        }, self.db.tables["Article"])
-        self.db.insert({
-            "id": 2,
-            "title": "TITLE2",
-            "status": "PUBLIC",
-            "media": "SECURITYMADEIN.LU",
-            "publication_date": datetime.date.today() - datetime.timedelta(days=1)
-        }, self.db.tables["Article"])
-        self.db.insert({
-            "id": 3,
-            "title": "TITLE3",
-            "status": "PUBLIC",
-            "media": "ALL",
-            "publication_date": datetime.date.today() - datetime.timedelta(days=1)
-        }, self.db.tables["Article"])
-        self.db.insert({
-            "id": 4,
-            "title": "TITLE4",
-            "status": "PUBLIC",
-            "media": "CYBERLUX",
-            "publication_date": datetime.date.today() - datetime.timedelta(days=1)
-        }, self.db.tables["Article"])
-
-        # Insertion of taxonomy
-
-        self.db.insert({"name": "CAT1"}, self.db.tables["TaxonomyCategory"])
-        self.db.insert({"id": 1, "name": "My Value", "category": "CAT1"}, self.db.tables["TaxonomyValue"])
-        self.db.insert({"article": 1, "taxonomy_value": 1}, self.db.tables["ArticleTaxonomyTag"])
-        self.db.insert({"article": 2, "taxonomy_value": 1}, self.db.tables["ArticleTaxonomyTag"])
-
-        # Insertion of company
-
-        self.db.insert({"id": 1, "name": "My Company"}, self.db.tables["Company"])
-        self.db.insert({"article": 1, "company": 1}, self.db.tables["ArticleCompanyTag"])
-        self.db.insert({"article": 3, "company": 1}, self.db.tables["ArticleCompanyTag"])
-        self.db.insert({"article": 4, "company": 1}, self.db.tables["ArticleCompanyTag"])
-
-        # Request
-
-        response = self.application.get('/public/get_related_articles/TITLE1?media=SECURITYMADEIN.LU')
-
-        self.assertEqual(200, response.status_code)
-        self.assertEqual([
-            {
-                'abstract': None,
-                'end_date': None,
-                'external_reference': None,
-                'handle': None,
-                'id': 2,
-                'image': None,
-                'link': None,
-                'media': 'SECURITYMADEIN.LU',
-                'publication_date': (datetime.date.today() - datetime.timedelta(days=1)).strftime("%Y-%m-%d"),
-                'start_date': None,
-                'status': 'PUBLIC',
-                'title': 'TITLE2',
-                'type': 'NEWS'
-            },
-            {
-                'abstract': None,
-                'end_date': None,
-                'external_reference': None,
-                'handle': None,
-                'id': 3,
-                'image': None,
-                'link': None,
-                'media': 'ALL',
-                'publication_date': (datetime.date.today() - datetime.timedelta(days=1)).strftime("%Y-%m-%d"),
-                'start_date': None,
-                'status': 'PUBLIC',
-                'title': 'TITLE3',
-                'type': 'NEWS'
-            }], response.json)
 
     def test_ko_no_article(self):
         response = self.application.get('/public/get_related_articles/TITLE1')
