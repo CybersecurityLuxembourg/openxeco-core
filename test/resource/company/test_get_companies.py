4be625542fe3cb3957cd78b58c53eb32f87b5d87
Alexis <alexis.prunier@securitymadein.lu>
2021-07-19 13:29:17 +0200

    Add unittest on company/get_companies with name param + Remove unused import

--- test/resource/company/test_get_companies.py
+++ test/resource/company/test_get_companies.py
@@ -13,3 +13,15 @@ class TestGetCompanies(BaseCase):
 
         self.assertEqual(2, len(response.json))
         self.assertEqual(200, response.status_code)
+
+    @BaseCase.login
+    def test_ok_with_params(self, token):
+        self.db.insert({"id": 2, "name": "My Company"}, self.db.tables["Company"])
+        self.db.insert({"id": 3, "name": "My Company 2"}, self.db.tables["Company"])
+
+        response = self.application.get('/company/get_companies?name=2',
+                                        headers=self.get_standard_header(token))
+
+        self.assertEqual(1, len(response.json))
+        self.assertEqual(3, response.json[0]["id"])
+        self.assertEqual(200, response.status_code)
