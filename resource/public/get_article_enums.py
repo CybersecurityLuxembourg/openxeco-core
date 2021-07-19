a689f2cd957b944627a80e5b3d51f53051dd69c8
Alexis <alexis.prunier@securitymadein.lu>
2021-07-19 14:28:43 +0200

    Correction of get_article_enum: token auth verification removed

--- resource/public/get_article_enums.py
+++ resource/public/get_article_enums.py
@@ -1,12 +1,10 @@
 from flask_apispec import MethodResource
 from flask_apispec import doc
-from flask_jwt_extended import jwt_required
 from flask_restful import Resource
 
 from db.db import DB
 from decorator.catch_exception import catch_exception
 from decorator.log_request import log_request
-from decorator.verify_admin_access import verify_admin_access
 
 
 class GetArticleEnums(MethodResource, Resource):
@@ -20,8 +18,6 @@ class GetArticleEnums(MethodResource, Resource):
          responses={
              "200": {},
          })
-    @jwt_required
-    @verify_admin_access
     @catch_exception
     def get(self):
 
