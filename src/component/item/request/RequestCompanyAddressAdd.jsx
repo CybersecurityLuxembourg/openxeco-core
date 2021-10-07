294649a741acf4a664d3e92fbbe40fce15a82c24
Alexis <alexis.prunier@securitymadein.lu>
2021-10-07 14:52:50 +0200

    Fix RequestCompanyAddressAdd that was not sending the modified data

--- src/component/item/request/RequestCompanyAddressAdd.jsx
+++ src/component/item/request/RequestCompanyAddressAdd.jsx
@@ -1,46 +1,14 @@
 import React, { Component } from "react";
 import "./RequestCompanyAddressAdd.css";
 import Popup from "reactjs-popup";
-import { NotificationManager as nm } from "react-notifications";
-import { postRequest } from "../../../utils/request.jsx";
 import Address from "../../button/Address.jsx";
 
 export default class RequestCompanyAddressAdd extends Component {
 	constructor(props) {
 		super(props);
 
-		this.insertAddress = this.insertAddress.bind(this);
-
 		this.state = {
 		};
-	}
-
-	insertAddress() {
-		if (this.props.data === undefined || this.props.data === null) {
-			nm.warning("Data to be inserted not found");
-			return;
-		}
-
-		if (this.props.data.id !== undefined) {
-			nm.warning("Cannot add an address with an ID");
-			return;
-		}
-
-		if (this.props.companyId === undefined || this.props.companyId === null) {
-			nm.warning("Company ID not found");
-			return;
-		}
-
-		const params = this.props.data;
-		params.company_id = this.props.companyId;
-
-		postRequest.call(this, "address/add_address", params, () => {
-			nm.info("The entity address has been added");
-		}, (response) => {
-			nm.warning(response.statusText);
-		}, (error) => {
-			nm.error(error.message);
-		});
 	}
 
 	render() {
@@ -64,26 +32,20 @@ export default class RequestCompanyAddressAdd extends Component {
 
 							<div className={"top-right-buttons"}>
 								<button
-									className={"red-background"}
+									className={"grey-background"}
 									onClick={close}>
-									<i className="fas fa-times"></i>
+									<i className="far fa-times-circle"></i>
 								</button>
 							</div>
 						</div>
 
 						<div className="col-md-12">
 							<Address
-								info={this.props.data}
+								info={{
+									...this.props.data,
+									...{ company_id: this.props.companyId },
+								}}
 							/>
-						</div>
-
-						<div className="col-md-12 right-buttons">
-							<button
-								className={"blue-background"}
-								onClick={this.insertAddress}
-							>
-								<i className="fas fa-plus"/> Add address
-							</button>
 						</div>
 					</div>
 				)}
