77c38d2aa42451bea55f27391f4a8925baa88cb7
Alexis <alexis.prunier@securitymadein.lu>
2021-10-08 16:36:02 +0200

    Remove unused import

--- resource/taxonomy/update_taxonomy_category.py
+++ resource/taxonomy/update_taxonomy_category.py
@@ -2,7 +2,6 @@ from flask_apispec import MethodResource
 from flask_apispec import use_kwargs, doc
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource
-from sqlalchemy.exc import IntegrityError
 from webargs import fields
 
 from decorator.catch_exception import catch_exception
