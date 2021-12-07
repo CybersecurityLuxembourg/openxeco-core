874873ab04c6de9ffb3e52ed7b28b13f9aca3cc9
Alexis <alexis.prunier@securitymadein.lu>
2021-12-07 14:58:43 +0100

    Add params to get users requiring assignment on user/get_users

--- resource/user/get_users.py
+++ resource/user/get_users.py
@@ -2,7 +2,7 @@ from flask_apispec import MethodResource
 from flask_apispec import use_kwargs, doc
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource
-from sqlalchemy import func
+from sqlalchemy import func, or_
 from webargs import fields, validate
 
 from db.db import DB
@@ -28,6 +28,7 @@ class GetUsers(MethodResource, Resource):
         'page': fields.Int(required=False, missing=1, validate=validate.Range(min=1)),
         'per_page': fields.Int(required=False, missing=50, validate=validate.Range(min=1, max=50)),
         'admin_only': fields.Bool(required=False),
+        'company_assignment_to_treat': fields.Bool(required=False),
     }, location="query")
     @jwt_required
     @verify_admin_access
@@ -38,7 +39,10 @@ class GetUsers(MethodResource, Resource):
             .with_entities(self.db.tables["User"].id,
                            self.db.tables["User"].email,
                            self.db.tables["User"].is_admin,
-                           self.db.tables["User"].is_active) \
+                           self.db.tables["User"].is_active,
+                           self.db.tables["User"].accept_communication,
+                           self.db.tables["User"].company_on_subscription,
+                           self.db.tables["User"].department_on_subscription) \
             .order_by(self.db.tables["User"].email.asc())
 
         if "email" in kwargs:
@@ -46,6 +50,12 @@ class GetUsers(MethodResource, Resource):
 
         if "admin_only" in kwargs and kwargs["admin_only"] is True:
             query = query.filter(self.db.tables["User"].is_admin.is_(True))
+
+        if "company_assignment_to_treat" in kwargs and kwargs["company_assignment_to_treat"] is True:
+            query = query.filter(or_(
+                self.db.tables["User"].company_on_subscription.isnot(None),
+                self.db.tables["User"].department_on_subscription.isnot(None)
+            ))
 
         paginate = query.paginate(kwargs['page'], kwargs['per_page'])
         users = [u._asdict() for u in paginate.items]
