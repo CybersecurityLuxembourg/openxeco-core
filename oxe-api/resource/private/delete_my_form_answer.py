3983b5455adddd3edda057fa5d6ff4c13e669ee5
Alexis <alexis.prunier@securitymadein.lu>
2022-04-27 14:19:59 +0200

    Become prospector compliant

--- oxe-api/resource/private/delete_my_form_answer.py
+++ oxe-api/resource/private/delete_my_form_answer.py
@@ -6,7 +6,6 @@ from webargs import fields
 
 from decorator.catch_exception import catch_exception
 from decorator.log_request import log_request
-from decorator.verify_admin_access import verify_admin_access
 from exception.object_not_found import ObjectNotFound
 
 
