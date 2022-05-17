d702c2406fdb1011bb3759b993391f025eca84ae
Alexis <alexis.prunier@securitymadein.lu>
2022-05-17 15:58:49 +0200

    Add public/get_public_article resource

---
+++ oxe-api/resource/public/get_public_article.py
@@ -0,0 +1,34 @@
+from flask_apispec import MethodResource
+from flask_apispec import doc
+from flask_restful import Resource
+
+from db.db import DB
+from decorator.catch_exception import catch_exception
+from decorator.log_request import log_request
+from exception.object_not_found import ObjectNotFound
+from utils.serializer import Serializer
+
+
+class GetPublicArticle(MethodResource, Resource):
+
+    def __init__(self, db: DB):
+        self.db = db
+
+    @log_request
+    @doc(tags=['public'],
+         description='Get an article by its ID',
+         responses={
+             "200": {},
+             "422": {"description": "Object not found"},
+         })
+    @catch_exception
+    def get(self, id_):
+
+        data = self.db.get(self.db.tables["Article"], {"id": id_})
+
+        if len(data) < 1:
+            raise ObjectNotFound
+
+        data = Serializer.serialize(data, self.db.tables["Article"])
+
+        return data[0], "200 "
