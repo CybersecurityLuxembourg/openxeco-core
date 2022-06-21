61da8dc13f38f134ecdc92528598e8e7678b920d
Alexis <alexis.prunier@securitymadein.lu>
2022-06-21 17:12:14 +0200

    Update version

--- oxe-api/resource/public/get_public_node_information.py
+++ oxe-api/resource/public/get_public_node_information.py
@@ -27,7 +27,7 @@ class GetPublicNodeInformation(MethodResource, Resource):
             "email_address": GetPublicNodeInformation.get_config(settings, "EMAIL_ADDRESS"),
             "phone_number": GetPublicNodeInformation.get_config(settings, "PHONE_NUMBER"),
             "postal_address": GetPublicNodeInformation.get_config(settings, "POSTAL_ADDRESS"),
-            "version": "1.10",
+            "version": "v1.11",
         })
 
     @staticmethod
