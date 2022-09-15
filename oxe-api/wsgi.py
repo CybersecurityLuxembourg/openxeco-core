cec530b273f916a323065e983714dcc05505a1f9
Alexis <alexis.prunier@securitymadein.lu>
2022-09-15 13:56:58 +0200

    ISSUE#46: Add "application" alias on app import on wsgi.py

--- oxe-api/wsgi.py
+++ oxe-api/wsgi.py
@@ -5,5 +5,5 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'venv', 'lib', 'python3.8', 'site-packages'))
 
 # pylint: disable=wrong-import-position
-from app import app
+from app import app as application
 assert app  # nosec
