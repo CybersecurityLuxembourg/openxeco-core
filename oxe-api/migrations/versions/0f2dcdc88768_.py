67594767ab40139562af6af9ac333b0118b6e69a
Alexis <alexis.prunier@securitymadein.lu>
2022-09-26 15:31:08 +0200

    Adjust database migration on Campaign table

--- oxe-api/migrations/versions/0f2dcdc88768_.py
+++ oxe-api/migrations/versions/0f2dcdc88768_.py
@@ -22,7 +22,9 @@ def upgrade():
     op.drop_column('Campaign', 'addresses')
     op.add_column('Campaign', sa.Column('template_id', mysql.INTEGER(), nullable=True))
     op.add_column('Campaign', sa.Column('execution_date', mysql.DATETIME(), nullable=True))
-    op.add_column('Campaign', sa.Column('name', mysql.mysql.VARCHAR(collation='utf8mb4_unicode_ci', length=200), nullable=True))
+    op.add_column('Campaign', sa.Column('name', mysql.VARCHAR(collation='utf8mb4_unicode_ci', length=200), nullable=True))
+    op.alter_column('Campaign', 'subject', type_=mysql.VARCHAR(charset='utf8mb4', collation='utf8mb4_unicode_ci', length=200), nullable=True)
+    op.alter_column('Campaign', 'body', type_=mysql.TEXT(collation='utf8mb4_unicode_ci'), nullable=True)
 
     op.create_table(
         'CampaignTemplate',
@@ -57,6 +59,8 @@ def downgrade():
     op.drop_column('Campaign', 'execution_date')
     op.drop_column('Campaign', 'name')
     op.add_column('Campaign', sa.Column('addresses', mysql.TEXT(collation='utf8mb4_unicode_ci'), nullable=True))
+    op.alter_column('Campaign', 'subject', type_=mysql.VARCHAR(charset='utf8mb4', collation='utf8mb4_unicode_ci', length=255), nullable=False)
+    op.alter_column('Campaign', 'body', type_=mysql.TEXT(collation='utf8mb4_unicode_ci'), nullable=False)
     op.rename_table('Campaign', 'Communication')
 
     op.drop_table('CampaignTemplate')
