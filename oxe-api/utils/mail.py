c0b53ccc6159bee506bf0d03d5726e121555ff1b
Kenny Crous <kenny@niu.com.mt>
2022-10-22 14:57:40 +0100

    Email Fixes

--- oxe-api/utils/mail.py
+++ oxe-api/utils/mail.py
@@ -1,4 +1,7 @@
+from email.mime.image import MIMEImage
 from flask_mail import Message
+
+
 
 
 def send_email(mail, subject, recipients, html_body, cc=None, bcc=None):  # pylint: disable=too-many-arguments
@@ -12,10 +15,22 @@ def send_email(mail, subject, recipients, html_body, cc=None, bcc=None):  # pyli
     )
     msg.html = html_body
 
+    with open("resource/static/eccclogo.png", 'rb') as f:
+        msg.attach(
+            "logo.png",
+            "image/png",
+            f.read(),
+            "inline",
+            headers=[
+                ['Content-ID','<logo>'],
+            ]
+        )
+
     try:
         mail.send(msg)
     except ConnectionRefusedError:
         raise Exception("An error occurred while connecting to the mail server")
+
 
 def send_email_with_attachment(mail, subject, recipients, html_body, file_name, file_type, cc=None, bcc=None):  # pylint: disable=too-many-arguments
     from config.config import MAIL_DEFAULT_SENDER  # pylint: disable=import-outside-toplevel
@@ -27,6 +42,18 @@ def send_email_with_attachment(mail, subject, recipients, html_body, file_name, 
         bcc=bcc
     )
     msg.html = html_body
+
+    with open("resource/static/eccclogo.png", 'rb') as f:
+        msg.attach(
+            "logo.png",
+            "image/png",
+            f.read(),
+            "inline",
+            headers=[
+                ['Content-ID','<logo>'],
+            ]
+        )
+
     with open(f"resource/static/{file_name}", 'rb') as f:
         msg.attach(file_name, file_type, f.read())
     try:
