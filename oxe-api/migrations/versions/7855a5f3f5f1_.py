dd13c996f674a37b32a17bcbdb56d4de3f502b9d
Alexis <alexis.prunier@securitymadein.lu>
2023-01-04 13:04:41 +0100

    Add EmailTemplate table in database structure

---
+++ oxe-api/migrations/versions/7855a5f3f5f1_.py
@@ -0,0 +1,33 @@
+"""empty message
+
+Revision ID: 7855a5f3f5f1
+Revises: 0f2dcdc88768
+Create Date: 2023-01-04 12:09:18.547169
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+
+# revision identifiers, used by Alembic.
+revision = '7855a5f3f5f1'
+down_revision = '0f2dcdc88768'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'EmailTemplate',
+        sa.Column('name', mysql.ENUM('ACCOUNT_CREATION', 'PASSWORD_RESET', 'REQUEST_NOTIFICATION'), nullable=False),
+        sa.Column('content', mysql.TEXT(charset='utf8mb4', collation='utf8mb4_unicode_ci'), nullable=True),
+        sa.PrimaryKeyConstraint('name'),
+        mysql_collate='utf8mb4_unicode_ci',
+        mysql_default_charset='utf8mb4',
+        mysql_engine='InnoDB'
+    )
+
+
+def downgrade():
+    op.drop_table('EmailTemplate')
